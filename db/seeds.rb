fe6c09c4564b1682846055e670fd91c56a1a3cf0
nbnp <nbnp11@gmail.com>
2019-02-16 15:50:42 +0300

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Building.create!(type: 'skyscraper',
-                  street: 'Pushkina',
-                  house_number: 1,
-                  floors: 100)
+               street: 'Pushkina',
+         house_number: 1,
+               floors: 100)
 
 Building.first.blocks.create!(area: 60, floor: 10)
 
 Block.first.offers.create!(offer_type: 'rent')
 
-Offer.first.price = Price.new(value: 500, currency: 1)
+Offer.last.create_price!(value: 500, currency: 1)
