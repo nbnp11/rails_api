293abd3ccce53dd6bb705be84a8eaeace7906d28
nbnp <nbnp11@gmail.com>
2019-02-16 16:51:27 +0300

    add create method and test

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,8 +1,16 @@
-class BuildingsController < ApplicationController
-  def create; end
-  def update; end
-  def delete; end
-  def show; end
-  def index; end
+# frozen_string_literal: true
 
+class BuildingsController < ApplicationController
+  def create
+    @building = Building.create!
+    render json: @building
+  end
+
+  def update; end
+
+  def delete; end
+
+  def show; end
+
+  def index; end
 end
--- spec/controllers/buildings_controller_spec.rb
+++ spec/controllers/buildings_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BuildingsController, type: :controller do
-
+  context 'test create method' do
+    it 'would return json on buildings#create' do
+      post :create
+      json_response = Building.last.to_json
+      expect(response.body).to eq(json_response)
+    end
+  end
 end
